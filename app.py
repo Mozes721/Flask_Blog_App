c739aa9345625af5355a55613a5b45c6840f2a79
Mozes721 <eric@mail.com>
2020-11-28 21:05:09 +0100

    registration
    update with registration

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
-from flask import Flask 
-from flask import render_template #render the html pages
-from flask import request #get user input from form
+from flask import Flask, render_template, request, session, redirect, url_for, flash
 import hashlib
-from flask_sqlalchemy import SQLAlchemy
-import psycopg2
-#to manage models in seperate file
+from datetime import timedelta
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
+#set the timeline the session can last
+app.permanent_session_lifetime = timedelta(days=5)
+SECRET_KEY = '\xb6\x05s\x15&p\xec\xb5qO4\xd5\xb5?\xf2\x87H\x11m`\xbb\xc5\x15P'
 
 #Configure Flask by providing the PostgreSQL URI 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Password@localhost/Database'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Asebomu12@localhost/Mozes721'
 #needs to be placed after app is created
-from models import db
+
+
+from models import db, UserInfo
 #migrate the app and the models 
 migrate = Migrate(app, db)
 #assign to flask_script manager the app
@@ -26,24 +27,94 @@ manager.add_command('db', MigrateCommand)
 
 @app.route('/', methods= ['post', 'get'])
 def index():
-    message = ''
+    #set the session as permanent with the given lifetime
+    session.permanent = True
     return render_template('index.html')
 
 #register route
-@app.route('/register', methods= ['post', 'get'])
+@app.route('/register', methods= ["POST", "GET"])
 def register():
-    return render_template('register.html')
+    if "username" in session:
+            username = session["username"]
+            flash("You are already logged in as %s" % username)
+            return render_template("register.html")
+    else:
+        if request.method == "POST":
+            email = request.form.get("email")
+            username = request.form.get("username")
+            password = request.form.get("password")
+            #if empty flash a message
+            if email == '' or username == '' or password == '':
+                flash("Please enter all input fields!")
+                return redirect(url_for("register"))
+            #check if user already in database
+            if db.session.query(UserInfo.id).filter_by(email = email).scalar() or db.session.query(UserInfo.id).filter_by(username = username).scalar() is not None:
+                flash("The email or username already is being used please choose a different one or login if your an existing user")
+                return redirect(url_for("register"))
+            #if not add the new user and redirect
+            register_user = UserInfo(email = email, username = username, password = password)
+            db.session.add(register_user)
+            db.session.commit()
+            #create a session while user logged in
+            session["username"] = username
+            flash("You have logged in as %s" % username)
+            return redirect(url_for('members', usr=username))
+        return render_template('register.html')
+
 
 #login route
 @app.route('/login', methods= ['post', 'get'])
 def login():
-    return render_template('login.html')
+    #check if user already in session(logged in)
+    if "username" in session:
+        username = session["username"]
+        flash("You are already logged in as %s" % username)
+        return render_template("login.html")
+    else:
+        if request.method == "POST":
+            print("Hello im posting!")
+            username = request.form.get("username")
+            password = request.form.get("password")
+            if username == '' or password == '':
+                flash("Please enter all input fields!")
+                return redirect(url_for("login"))
+            #check if user already exists in database
+            if db.session.query(UserInfo.id).filter_by(username = username).scalar():
+                #check if password match specific user
+                if db.session.query(UserInfo.id).filter_by(password = password).scalar():
+                    session["username"] = username
+                    flash("You have logged in as %s" % username)
+                    return redirect(url_for('members'))
+                flash("Password is incorect please try again")
+                return redirect(url_for("login"))
+            flash("Username is incorect please try again")
+            return redirect(url_for("login"))
+        return render_template('login.html')
 
-#logout route
-@app.route('/logout', methods= ['post', 'get'])
+
+
+
+#user_form route
+@app.route('/user', methods= ['post', 'get'])
+def members():
+    #only if user is in session otherwise redirect
+    if "username" in session:
+        username = session["username"]
+        flash("You have logged in as %s" % username)
+        return render_template('members.html')
+    else:
+        return redirect(url_for("index"))
+
+
+
+@app.route('/logout')
 def logout():
-    return render_template('logout.html')
+    session.pop("user", None)
+    flash("You have been logged out!", "info")
+    return redirect(url_for("index"))
+        
 
 #detects changes and updates on relode
 if __name__ == '__main__':
+    app.config['SECRET_KEY'] = SECRET_KEY
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from __main__ import app
 
+
 db = SQLAlchemy(app)
 
 class UserInfo(db.Model):
@@ -8,8 +9,8 @@ class UserInfo(db.Model):
 
     __tablename__ = "userInfo"
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(25), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(25), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
 
     def __init__(self, username, email, password):
@@ -17,14 +18,18 @@ class UserInfo(db.Model):
         self.email = email
         self.password = password
 
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
 # class Blogs(db.Model):
 #     pass 
 
 # class User_blog(db.Model):
 #     pass
 
-# def __repr__(self):
-#     return '<User {}>'.format(self.username)
+
+
+
 
 
 db.create_all()
